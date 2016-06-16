b2f4d047a336502e343257aa9269faaf5b87ebf5
jeongjieun <tladmsal147@naver.com>
2016-06-17 01:27:48 +0900

    shooting game

---
+++ New Unity Project 8/Assets/scripts/bulletmove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletmove : MonoBehaviour {
+    public float moveSpeed = 10f;
+    public float destoryTime = 2f;
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+	}
+}
